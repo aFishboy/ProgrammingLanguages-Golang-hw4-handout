c81eaabd12b0c49bb13734903ab1669ad2c568ef
aFishboy <samuelhoward914@gmail.com>
2023-06-03 15:26:26 -0700

    bug2 done

--- bug2/bug2.go
+++ bug2/bug2.go
@@ -8,10 +8,10 @@ func bug2(n int, foo func(int) int, out chan int) {
 	var wg sync.WaitGroup
 	for i := 0; i < n; i++ {
 		wg.Add(1)
-		go func() {
+		go func(i int) {
 			out <- foo(i)
 			wg.Done()
-		}()
+		}(i)
 	}
 	wg.Wait()
 	close(out)
