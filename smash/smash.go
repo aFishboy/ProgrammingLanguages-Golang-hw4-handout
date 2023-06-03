f401a40b380c23c3f11dab5819bb7ca0132f5232
aFishboy <samuelhoward914@gmail.com>
2023-06-03 16:51:45 -0700

    smash done

--- smash/smash.go
+++ smash/smash.go
@@ -1,13 +1,49 @@
 package smash
 
 import (
+	"bufio"
 	"io"
+	"strings"
+	"sync"
 )
 
 type word string
 
-func Smash(r io.Reader, smasher func(word) uint32) map[uint32]uint {
-	m := make(map[uint32]uint)
-	// TODO: Incomplete!
-	return m
+func Smash(reader io.Reader, smasher func(word) uint32) map[uint32]uint {
+	smashMap := make(map[uint32]uint)
+	scanner := bufio.NewScanner(reader)
+	var wg sync.WaitGroup
+	var mutex sync.Mutex
+
+	lines := make(chan string) 
+
+	go func() {
+		defer close(lines)
+		for scanner.Scan() {
+			lines <- scanner.Text()
+		}
+	}()
+
+	for line := range lines {
+		wg.Add(1)
+
+		go func(line string) {
+			defer wg.Done()
+
+			words := strings.Fields(line)
+
+			for _, w := range words {
+				word := word(w)
+				hash := smasher(word)
+
+				mutex.Lock()
+				smashMap[hash]++
+				mutex.Unlock()
+			}
+		}(line)
+	}
+
+	wg.Wait()
+
+	return smashMap
 }
